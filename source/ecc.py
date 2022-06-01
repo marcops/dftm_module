de638975b2bade569f21d5321a31c79678bf45e5
Marco <marcolikis@gmail.com>
2022-06-01 09:56:33 -0300

    vhdl performance improve

--- source/ecc.py
+++ source/ecc.py
@@ -187,74 +187,85 @@ class ecc():
 
         if type == ECC_LPC_WITHOUT_PARITY:
             d = data[WORD_SIZE:]
-            
-            sR02 = data[16] ^ ((d[0]^d[1])^d[3])
-            sR01 = data[17] ^ ((d[0]^d[2])^d[3])
-            sR00 = data[18] ^ ((d[1]^d[2])^d[3])
-            sR12 = data[19] ^ ((d[4]^d[5])^d[7])
-            sR11 = data[20] ^ ((d[4]^d[6])^d[7])
-            sR10 = data[21] ^ ((d[5]^d[6])^d[7])
-            sR22 = data[22] ^ ((d[8]^d[9])^d[11])
-            sR21 = data[23] ^ ((d[8]^d[10])^d[11])
-            sR20 = data[24] ^ ((d[9]^d[10])^d[11])
-            sR32 = data[25] ^ ((d[12]^d[13])^d[15])
-            sR31 = data[26] ^ ((d[12]^d[14])^d[15])
-            sR30 = data[27] ^ ((d[13]^d[14])^d[15])
+            sR0 = intbv(bool(0))[3:]
+            sR1 = intbv(bool(0))[3:]
+            sR2 = intbv(bool(0))[3:]
+            sR3 = intbv(bool(0))[3:]
+            sC0 = intbv(bool(0))[3:]
+            sC1 = intbv(bool(0))[3:]
+            sC2 = intbv(bool(0))[3:]
+            sC3 = intbv(bool(0))[3:]
 
-            sC02 = data[28] ^ ((d[0]^d[4])^d[12])
-            sC01 = data[29] ^ ((d[0]^d[8])^d[12])
-            sC00 = data[30] ^ ((d[4]^d[8])^d[12])
-            sC12 = data[31] ^ ((d[1]^d[5])^d[13])
-            sC11 = data[32] ^ ((d[1]^d[9])^d[13])
-            sC10 = data[33] ^ ((d[5]^d[9])^d[13])
-            sC22 = data[34] ^ ((d[2]^d[6])^d[14])
-            sC21 = data[35] ^ ((d[2]^d[10])^d[14])
-            sC20 = data[36] ^ ((d[6]^d[10])^d[14])
-            sC32 = data[37] ^ ((d[3]^d[7])^d[15])
-            sC31 = data[38] ^ ((d[3]^d[11])^d[15])
-            sC30 = data[39] ^ ((d[7]^d[11])^d[15])
+            sR = intbv(bool(0))[4:]
+            sC = intbv(bool(0))[4:]
 
-            sR0 = sR00 | sR01 | sR02
-            sR1 = sR10 | sR11 | sR12
-            sR2 = sR20 | sR21 | sR22
-            sR3 = sR30 | sR31 | sR32
+            sR0[2] = data[16] ^ ((d[0]^d[1])^d[3])
+            sR0[1] = data[17] ^ ((d[0]^d[2])^d[3])
+            sR0[0] = data[18] ^ ((d[1]^d[2])^d[3])
+            sR1[2] = data[19] ^ ((d[4]^d[5])^d[7])
+            sR1[1] = data[20] ^ ((d[4]^d[6])^d[7])
+            sR1[0] = data[21] ^ ((d[5]^d[6])^d[7])
+            sR2[2] = data[22] ^ ((d[8]^d[9])^d[11])
+            sR2[1] = data[23] ^ ((d[8]^d[10])^d[11])
+            sR2[0] = data[24] ^ ((d[9]^d[10])^d[11])
+            sR3[2] = data[25] ^ ((d[12]^d[13])^d[15])
+            sR3[1] = data[26] ^ ((d[12]^d[14])^d[15])
+            sR3[0] = data[27] ^ ((d[13]^d[14])^d[15])
 
-            sC0 = sC00 | sC01 | sC02
-            sC1 = sC10 | sC11 | sC12
-            sC2 = sC20 | sC21 | sC22
-            sC3 = sC30 | sC31 | sC32
+            sC0[2] = data[28] ^ ((d[0]^d[4])^d[12])
+            sC0[1] = data[29] ^ ((d[0]^d[8])^d[12])
+            sC0[0] = data[30] ^ ((d[4]^d[8])^d[12])
+            sC1[2] = data[31] ^ ((d[1]^d[5])^d[13])
+            sC1[1] = data[32] ^ ((d[1]^d[9])^d[13])
+            sC1[0] = data[33] ^ ((d[5]^d[9])^d[13])
+            sC2[2] = data[34] ^ ((d[2]^d[6])^d[14])
+            sC2[1] = data[35] ^ ((d[2]^d[10])^d[14])
+            sC2[0] = data[36] ^ ((d[6]^d[10])^d[14])
+            sC3[2] = data[37] ^ ((d[3]^d[7])^d[15])
+            sC3[1] = data[38] ^ ((d[3]^d[11])^d[15])
+            sC3[0] = data[39] ^ ((d[7]^d[11])^d[15])
 
-            if sR0 == 1 and sC0 == 1:
+            sR[0] = sR0[2] | sR0[1] | sR0[0]
+            sR[1] = sR1[2] | sR1[1] | sR1[0]
+            sR[2] = sR2[2] | sR2[1] | sR2[0]
+            sR[3] = sR3[2] | sR3[1] | sR3[0]
+
+            sC[0] = sC0[2] | sC0[1] | sC0[0]
+            sC[1] = sC1[2] | sC1[1] | sC1[0]
+            sC[2] = sC2[2] | sC2[1] | sC2[0]
+            sC[3] = sC3[2] | sC3[1] | sC3[0]
+
+            if sR[0] == 1 and sC[0] == 1:
                 d[0] = ~d[0]
-            if sR0 == 0 and sC1 == 1:
+            if sR[0] == 0 and sC[1] == 1:
                 d[1] = ~d[1]
-            if sR0 == 0 and sC2 == 1:
+            if sR[0] == 0 and sC[2] == 1:
                 d[2] = ~d[2]
-            if sR0 == 0 and sC3 == 1:
+            if sR[0] == 0 and sC[3] == 1:
                 d[3] = ~d[3]
-            if sR1 == 1 and sC0 == 1:
+            if sR[1] == 1 and sC[0] == 1:
                 d[4] = ~d[4]
-            if sR1 == 1 and sC1 == 1:
+            if sR[1] == 1 and sC[1] == 1:
                 d[5] = ~d[5]
-            if sR1 == 1 and sC2 == 1:
+            if sR[1] == 1 and sC[2] == 1:
                 d[6] = ~d[6]
-            if sR1 == 1 and sC3 == 1:
+            if sR[1] == 1 and sC[3] == 1:
                 d[7] = ~d[7]
-            if sR2 == 1 and sC0 == 1:
+            if sR[2] == 1 and sC[0] == 1:
                 d[8] = ~d[8]
-            if sR2 == 1 and sC1 == 1:
+            if sR[2] == 1 and sC[1] == 1:
                 d[9] = ~d[9]
-            if sR2 == 1 and sC2 == 1:
+            if sR[2] == 1 and sC[2] == 1:
                 d[10] = ~d[10]
-            if sR2 == 1 and sC3 == 1:
+            if sR[2] == 1 and sC[3] == 1:
                 d[11] = ~d[11]
-            if sR3 == 1 and sC0 == 1:
+            if sR[3] == 1 and sC[0] == 1:
                 d[12] = ~d[12]
-            if sR3 == 1 and sC1 == 1:
+            if sR[3] == 1 and sC[1] == 1:
                 d[13] = ~d[13]
-            if sR3 == 1 and sC2 == 1:
+            if sR[3] == 1 and sC[2] == 1:
                 d[14] = ~d[14]
-            if sR3 == 1 and sC3 == 1:
+            if sR[3] == 1 and sC[3] == 1:
                 d[15] = ~d[15]
            
             return d
